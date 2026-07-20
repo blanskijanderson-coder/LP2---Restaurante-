c89addc9d451346c242566b63435185de6afb51e
Marcelo Livros <marceloliryo@gmail.com>
2026-07-20 13:59:30 -0300

    Atualizado main.java funcional

--- src/restaurante/Main.java
+++ src/restaurante/Main.java
@@ -1,44 +1,29 @@
 package restaurante;
 
-import java.util.ArrayList;
+import java.awt.EventQueue;
+import javax.swing.UIManager;
+import restaurante.view.Inicio;
 
 public class Main {
 
     public static void main(String[] args) {
-        // Criando alguns produtos iniciais
-        new Comida("Lasanha à Bolonhesa", 45.0, "Massa artesanal", "Trigo, carne, queijo e molho");
-        new Comida("Filé com Fritas", 38.0, "Carne suculenta", "Carne bovina, batata, sal");
-        new Bebida("Suco de Uva", 12.0, "Fazenda Videira");
-        new Bebida("Coca-Cola 350ml", 7.0, "Distribuidora Regional");
+        configurarTema();
 
-        Cliente c1 = new Cliente("Janderson", "janderson@uneb.br", "123456789", "teste".toCharArray(), false);
+        EventQueue.invokeLater(() -> {
+            new Inicio().setVisible(true);
+        });
+    }
 
-        System.out.println("======= BEM-VINDO AO RESTAURANTE =======");
-
-        // 1. Escolher Mesa
-        System.out.println("\n--- Escolhendo Mesa ---");
-        c1.escolherMesa();
-
-        // 2. Ver Cardápio
-        System.out.println("\n--- Cardápio ---");
-        c1.verCardapio();
-
-        // 3. Realizando o Pedido
-        System.out.println("\n--- Realizando o Pedido ---");
-
-        // Simulando a escolha de itens do cardápio
-
-        // 4. Painel da Cozinha
-        System.out.println("\n--- Painel da Cozinha ---");
-        Cozinha.listarPedido();
-
-        System.out.println("\n--- Preparando e Entregando Prato ---");
-        Cozinha.entregarPedido(0);
-
-        // 5. Área de Pagamento
-        System.out.println("\n--- Área de Pagamento ---");
-        c1.pagarConta();
-
-        System.out.println("\n======= FIM DA SIMULAÇÃO =======");
+    private static void configurarTema() {
+        try {
+            for (UIManager.LookAndFeelInfo info : UIManager.getInstalledLookAndFeels()) {
+                if ("Nimbus".equals(info.getName())) {
+                    UIManager.setLookAndFeel(info.getClassName());
+                    break;
+                }
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 }
